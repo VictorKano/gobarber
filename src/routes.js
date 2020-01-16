622f6c092c33f8eeb4d3e93c495042ca98abe877
Victor Kano <victorkano27@gmail.com>
2020-01-16 09:05:12 -0300

    loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import Router from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Victor Isao Kano',
+    email: 'victor_kano@outlook.com',
+    password_hash: '12352421',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
