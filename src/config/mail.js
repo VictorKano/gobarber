94a5d03a2a4e5c0918151ff2f66debcf9af94bb3
Victor Kano <victorkano27@gmail.com>
2020-01-20 09:28:18 -0300

    configurado nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3af6e87f374bd7',
+    pass: '16d48447a01110',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
